4f87a12d5d941a27f1fd9b96d51b7a102de8ac65
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2024-09-01 17:57:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -530,9 +530,6 @@ def bokeh_fig():
     year_counts.columns = ['Year', 'Number of Stations']
     year_counts = year_counts.sort_values(by='Year')
 
-    # Convert the data to a Bokeh ColumnDataSource
-    #source = ColumnDataSource(year_counts)
-
     # Create a Bokeh figure
     p = figure(
         title='Number of Shinkansen Stations Established by Year',
@@ -542,9 +539,12 @@ def bokeh_fig():
         height=400
     )
 
+    x = year_counts['Year']
+    y = year_counts['Number of Stations']
+
     # Add a line and circle glyph
-    p.line(x='Year', y='Number of Stations', source=year_counts, line_width=2, color='blue', legend_label='Stations')
-    p.circle(x='Year', y='Number of Stations', source=year_counts, size=8, color='red', legend_label='Stations')
+    p.line(x=x, y=y, line_width=2, color='blue', legend_label='Stations')
+    p.circle(x=x, y=y, source=source, size=8, color='red', legend_label='Stations')
 
 
     # Customize the chart
